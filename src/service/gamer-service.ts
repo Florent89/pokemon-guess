afd104a26f489c7e84abaa05ed547507d350a250
Florent <floderouet@laposte.net>
2023-03-29 15:15:11 +0200

    add process env node control

--- src/service/gamer-service.ts
+++ src/service/gamer-service.ts
@@ -9,47 +9,50 @@ export type Gamer = {
 export default class GamerService {
   static gamers: Gamer[] = [];
 
-  //   static isDev =
-  //     !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+  static isDev =
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
   static getGamers(): Promise<Gamer[]> {
-    return fetch("http://localhost:3001/gamers")
-      .then((response) => response.json())
-      .catch((error) => this.handleError(error));
-
-    // return new Promise((resolve) => {
-    //   resolve(this.pokemons);
-    // });
+    if (this.isDev) {
+      return fetch("http://localhost:3001/gamers")
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
+    return new Promise((resolve) => {
+      resolve(this.gamers);
+    });
   }
 
   static addGamer(gamer: Gamer): Promise<Gamer> {
     gamer.created = new Date(gamer.created);
-
-    return fetch(`http://localhost:3001/gamers`, {
-      method: "POST",
-      body: JSON.stringify(gamer),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((response) => response.json())
-      .catch((error) => this.handleError(error));
-
-    // return new Promise((resolve) => {
-    //   this.pokemons.push(pokemon);
-    //   resolve(pokemon);
-    // });
+    if (this.isDev) {
+      return fetch(`http://localhost:3001/gamers`, {
+        method: "POST",
+        body: JSON.stringify(gamer),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
+    return new Promise((resolve) => {
+      this.gamers.push(gamer);
+      resolve(gamer);
+    });
   }
 
   static searchGamer(term: string): Promise<Gamer[]> {
-    return fetch(`http://localhost:3001/gamers?q=${term}`)
-      .then((response) => response.json())
-      .catch((error) => this.handleError(error));
+    if (this.isDev) {
+      return fetch(`http://localhost:3001/gamers?q=${term}`)
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
 
-    // return new Promise((resolve) => {
-    //   const results = this.pokemons.filter((pokemon) =>
-    //     pokemon.name.includes(term)
-    //   );
-    //   resolve(results);
-    // });
+    return new Promise((resolve) => {
+      const results = this.gamers.filter((gamer) => {
+        gamer.pseudo.includes(term) || gamer.level.includes(term);
+      });
+      resolve(results);
+    });
   }
 
   static handleError(error: Error): void {
